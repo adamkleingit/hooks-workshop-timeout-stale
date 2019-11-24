059a6f987eadf2f53ed3d3878b6e72a326810f4b
adam klein <adam@500tech.com>
2019-11-24 09:58:41 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# hooks-workshop-timeout-stale
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  const [count, setCount] = useState(1);
+  const [timerOn, setTimerOn] = useState(false);
+
+  const increment = () => {
+    if (timerOn) {
+      let interval = setInterval(() => {
+        setCount(count + 1);
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  };
+
+  useEffect(increment, [timerOn]);
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={() => setTimerOn(prev => !prev)}>
+        {timerOn ? "Pause" : "Play"}
+      </button>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
